13e8a474b1dd453f0d2960c9e408c08db9800e8e
josseycodes1 <adewumijosephine1@gmail.com>
2025-12-11 01:28:38 +0100

    refactor(auth): added wallet auth to google auth

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,4 @@ from . import views
 urlpatterns = [
     path('google/', views.GoogleAuthRedirectView.as_view(), name='google-auth'),
     path('google/callback/', views.GoogleAuthCallbackView.as_view(), name='google-callback'),
-    path('token/', views.JWTTokenObtainView.as_view(), name='token-obtain'),
-    path('profile/', views.UserProfileView.as_view(), name='user-profile'),
 ]
