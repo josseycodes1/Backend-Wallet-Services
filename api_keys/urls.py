c9685a061d8f60e19e34503e206f1f94da5d090d
josseycodes1 <adewumijosephine1@gmail.com>
2025-12-11 02:12:38 +0100

    fix(API KEY)- removed unecessary api key endpoints

--- api_keys/urls.py
+++ api_keys/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('list/', views.APIKeyListView.as_view(), name='api-key-list'),
     path('rollover/', views.RolloverAPIKeyView.as_view(), name='api-key-rollover'),
     path('revoke/', views.RevokeAPIKeyView.as_view(), name='api-key-revoke'),
-    path('<uuid:pk>/update/', views.UpdateAPIKeyView.as_view(), name='api-key-update'),
 ]
